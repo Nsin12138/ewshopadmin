840e0d21d3c4d625db3fe4af6980659d311e8391
牛鑫 <1248556224@qq.com>
2022-09-01 21:39:18 +0800

    首页统计功能完善

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,55 +1,147 @@
 <template>
-  <div class="zero flex justify-between">
-    <div class="one bg-white">
-        <div class="font-s">
-          用户数
-        </div>
-        <div style="color: rgb(63, 134, 0);font-size: 24px">
-          {{users_count}}
-        </div>
+  <div class="content px-4">
+    <div class="mt-4 bg-white">
+      <div>
+        <n-card class="xt"
+            style="border: 1px solid #bed0ea"
+            title="系统信息"
+            :bordered="true"
+            :segmented="{
+                content: true,
+                footer: 'soft'
+            }"
+            header-style="background-color:#f9fbfe;padding:10px;border-bottom:1px solid #bed0ea"
+
+
+        >
+          <p v-for="item in Systems" :key="item.id">{{item.title}}：<a :href="item.url">{{item.value}}</a></p>
+<!--          <p>开发团队：<a href="https://www.lmonkey.com/">学习猿地</a> </p>
+          <p>邮箱：<u>ewshop@eduwork.cn</u></p>
+          <p>Q Q：<a href="#"> 779050720</a></p>-->
+        </n-card>
+      </div>
     </div>
-    <div class="two bg-white">
-      <div class="font-s">
-        商品数
-      </div>
-      <div style="color: rgb(207, 19, 34);font-size: 24px"  >
-        {{goods_count}}
-      </div>
+  </div>
+  <div class="flex">
+    <div style="width: 48%" class="mt-4 bg-white ml-5">
+      <n-card
+          style="border: 1px solid #bed0ea"
+          title="商品信息"
+          :segmented="{
+      content: true,
+      footer: 'soft',
+    }"
+          header-style="background-color:#f9fbfe;padding:10px;border-bottom:1px solid #bed0ea"
+          content-style="padding:5px 20px"
+      >
+        <div style="display: flex" class="goods">
+          <div style="width: 50%">
+            <div  v-for="item in stat">
+              <span >{{item.title}}</span>
+            </div>
+          </div>
+          <div style="width: 50%">
+            <div style="text-align: right"  v-for="item in goods_info">
+              <b style="text-align: right;color:green;">{{item}}</b>件
+            </div>
+          </div>
+        </div>
+      </n-card>
 
     </div>
-    <div  class="three bg-white">
-      <div class="font-s">
-        订单数
-      </div>
-      <div style="color: rgb(0, 59, 128);font-size: 24px">
-        {{order_count}}
+    <div style="width: 48%" class="mt-4 bg-white ml-5">
+      <n-card
+          style="border: 1px solid #bed0ea"
+          title="访客统计"
+          :segmented="{
+      content: true,
+      footer: 'soft',
+    }"
+          header-style="background-color:#f9fbfe;padding:10px;border-bottom:1px solid #bed0ea"
+          content-style="padding:5px 20px"
+      >
+        <div style="display: flex" class="goods">
+          <div style="width: 50%">
+            <div  v-for="item in stat1">
+              <span >{{item.title}}</span>
+            </div>
+          </div>
+          <div style="width: 50%">
+            <div style="text-align: right"  v-for="item in users_info">
+              <b style="text-align: right;color:green;">{{item}}</b>件
+            </div>
+          </div>
+        </div>
+      </n-card>
+
+    </div>
+  </div>
+  <div class="content px-4">
+    <div class="mt-4 bg-white">
+      <div>
+        <n-card class="xt"
+                style="border: 1px solid #bed0ea"
+                title="交易数据"
+                :bordered="true"
+                :segmented="{
+                content: true,
+                footer: 'soft'
+            }"
+                header-style="background-color:#f9fbfe;padding:10px;border-bottom:1px solid #bed0ea"
+
+
+        >
+          <p v-for="item in Systems" :key="item.id">{{item.title}}：<a :href="item.url">{{item.value}}</a></p>
+          <!--          <p>开发团队：<a href="https://www.lmonkey.com/">学习猿地</a> </p>
+                    <p>邮箱：<u>ewshop@eduwork.cn</u></p>
+                    <p>Q Q：<a href="#"> 779050720</a></p>-->
+        </n-card>
       </div>
     </div>
   </div>
 
-
 </template>
 
 <script lang="ts" setup>
-import {reactive, onMounted, toRefs} from "vue";
+import {onMounted, ref,toRefs} from "vue";
 import {stats} from "@/api/stat";
 
-const stat = reactive({
-  users_count:'',
-  goods_count:'',
-  order_count:'',
-})
-const {users_count,goods_count,order_count} = toRefs(stat)
+const goods_info = ref([])
+const users_info = ref([])
+const order_info = ref([])
+
+const stat = [
+  {id:1,title:'上架商品'},
+  {id:2,title:'下架商品'},
+  {id:3,title:'缺货商品'},
+  {id:4,title:'推荐商品数量'}
+]
+const stat1 = [
+  {id:1,title:'今日商品'},
+  {id:2,title:'昨日商品'},
+  {id:3,title:'累计访客'},
+  {id:4,title:'累计注册'}
+]
+
 const getNum = (params) =>{
-  stats(params).then(stats => {
-    users_count.value = stats.users_count
-    goods_count.value = stats.goods_count
-    order_count.value = stats.order_count
+  stats(params).then(res => {
+    goods_info.value = res.goods_info
+    users_info.value = res.users_info
+    order_info.value = res.order_info
   })
 }
+
 onMounted(()=>{
   getNum({})
 })
+
+const Systems = [
+    {id:1,title:'软件版本',value:'EWShop1.0教学版',url:"https://www.eduwork.cn/"},
+    {id:2,title:'开发团队',value:'学习猿地',url:"https://www.lmonkey.com/"},
+    {id:3,title:'邮箱',value:'ewshop@eduwork.cn',url: '#'},
+    {id:4,title:'Q Q',value:'779050720',url:'#'},
+  ]
+
 
 
 
@@ -59,22 +151,34 @@ onMounted(()=>{
 </script>
 
 <style scoped >
-.zero{
-  margin: 24px;
+
+.n-card__content{
+  padding-top: 5px;
 }
-.font-s{
-  margin-bottom: 4px;
-  color: rgba(0,0,0,.45);
-  font-size: 14px;
+.n-card__content>p>a{
+  text-decoration: underline;
 }
-.one,.two,.three {
-  display: block;
-  height: 100px;
-  flex: 0 0 33.33333333%;
-  max-width: 33.33333333%;
-  margin-left: 8px;
-  margin-right: 8px;
-  padding: 24px;
+.n-card__content>p>a:hover{
+  color: orangered;
+}
+.n-card__content>p:first-child>a{
+  color:green;
+  text-decoration: none;
+}
+.n-card__content>p:first-child:after{
+  content: '（证书编号：无）';
+  color: orangered;
+  text-decoration: none;
+}
+.goods > div >div:not(:last-child){
+  border-bottom: 1px dashed #ccc;
+}
+.goods > div > div {
+  height: 50px;
+  line-height: 50px;
+}
+.n-card > .n-card-header{
+  padding: 10px;
 }
 
 </style>
